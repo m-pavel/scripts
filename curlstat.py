82615d18312d1e7d79a891c2d8e6d9fc86a5a8c2
paullus <paullus@hyrax>
2014-04-23 01:01:19 +0300

    fix

--- curlstat.py
+++ curlstat.py
@@ -10,10 +10,11 @@ rrd_file = "/tmp/spped.rrd"
 def chunk_report(bytes_so_far, chunk_size, total_size, time_start, time_prev, time_curr):
     percent = float(bytes_so_far) / total_size
     percent = round(percent*100, 2)
-    av_speed = bytes_so_far / 1024 / (time_curr - time_start)
+    av_speed = float(bytes_so_far) / 1024 / (time_curr - time_start)
     react_speed = chunk_size / 1024 / (time_curr - time_prev)
     print '{0} and {1} and {2}'.format(av_speed, react_speed, percent)
-    rrdtool.update(rrd_file, 'N:%s:%s:%s' % (av_speed, react_speed, percent))
+#    rrdtool.update(rrd_file, 'N:%s:%s:%s:%s' % (av_speed, react_speed, percent, bytes_so_far))
+    rrdtool.update(rrd_file, 'N:%s:%s' % (av_speed, bytes_so_far))
 
 def chunk_read(response, chunk_size=8192, report_hook=None):
     total_size = response.info().getheader('Content-Length').strip()
@@ -34,21 +35,34 @@ def chunk_read(response, chunk_size=8192, report_hook=None):
     return bytes_so_far
 
 if __name__ == '__main__':
-    data_sources=[ 'DS:speed_av:GAUGE:600:U:U','DS:speed_react:GAUGE:600:U:U','DS:percent:GAUGE:600:U:U','RRA:AVERAGE:0.5:1:600' ]
-    rrdtool.create(rrd_file, data_sources)
-    response = urllib2.urlopen(url);
-    chunk_read(response, 1024, report_hook=chunk_report)
 
-    ret = rrdtool.graph( output, "--start", "-2d", "--vertical-label=KB/s",
+    data_sources=[  'DS:speed_av:GAUGE:10:U:U',
+#		    'DS:speed_react:GAUGE:10:U:U',
+#		    'DS:percent:GAUGE:10:U:U',
+		    'DS:amount:COUNTER:10:U:U',
+		    'RRA:AVERAGE:0.5:10:1000' ,
+		    'RRA:AVERAGE:0.5:5:1000' ,
+		    'RRA:AVERAGE:0.5:1:1000',
+		    'RRA:LAST:0.5:2:1000']
+    rrdstart = int(time.time()) 
+    rrdtool.create(rrd_file, '--step', '1', '--start', str(rrdstart), data_sources)
+    response = urllib2.urlopen(url);
+    chunk_read(response, 2048, report_hook=chunk_report)
+    rrdend = int(time.time())
+    print (rrdend - rrdstart)
+    ret = rrdtool.graph( output, "--start", str(rrdstart-10) , "--end", str(rrdend+10),
+"--vertical-label=KB/s",
 '--watermark=stat',
 "-w 800",
 "DEF:m1_num="+rrd_file+":speed_av:AVERAGE",
-"DEF:m2_num="+rrd_file+":speed_react:AVERAGE",
-"LINE1:m1_num#0000FF:speed_av\\r",
-"LINE2:m2_num#00FF00:speed_react\\r",
-"GPRINT:m1_num:AVERAGE:Avg speed\: %6.0lf ",
-"GPRINT:m1_num:MAX:Max speed\: %6.0lf \\r",
-"GPRINT:m2_num:AVERAGE:Avg react speed\: %6.0lf ",
-"GPRINT:m2_num:MAX:Max react speed\: %6.0lf \\r")
+"DEF:m2_num="+rrd_file+":amount:AVERAGE",
+"DEF:m3_num="+rrd_file+":speed_av:LAST",
+#"LINE1:m1_num#0000FF:speed_av\\r",
+"LINE2:m2_num#00FF00:amount\\r",
+#"LINE3:m3_num#FF0000:last\\r",
+#"GPRINT:m1_num:AVERAGE:Avg speed\: %6.0lf ",
+#"GPRINT:m1_num:MAX:Max speed\: %6.0lf \\r")
+"GPRINT:m3_num:AVERAGE:Avg\: %6.0lf ",
+"GPRINT:m3_num:MAX:Max\: %6.0lf \\r")
 
 print ret
